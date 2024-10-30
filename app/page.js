4acd56ae340ad448b7ffe14c0867ffb7282997c7
AndyNg555 <ngquan555@gmail.com>
2024-10-30 01:42:45 -0600

    fix main page.js

--- app/page.js
+++ app/page.js
@@ -7,6 +7,10 @@ function App() {
       <ul>
         <li><a href="/week-2">Week 2</a></li>
         <li><a href="/week-3">Week 3</a></li>
+        <li><a href="/week-4">Week 4</a></li>
+        <li><a href="/week-5">Week 5</a></li>
+        <li><a href="/week-6">Week 6</a></li>
+        <li><a href="/week-7">Week 7</a></li>
       </ul>
     </div>
   );
