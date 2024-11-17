b7a6c0806a1eb116cf46948addeb11ed67be0106
AndyNg555 <ngquan555@gmail.com>
2024-11-16 19:05:25 -0700

    page dir added

--- app/page.js
+++ app/page.js
@@ -11,6 +11,10 @@ function App() {
         <li><a href="/week-5">Week 5</a></li>
         <li><a href="/week-6">Week 6</a></li>
         <li><a href="/week-7">Week 7</a></li>
+        <li><a href="/week-8">Week 8</a></li>
+        <li><a href="/week-9">Week 9</a></li>
+        <li><a href="/week-10">Week 10</a></li>
+        <li><a href="/week-11">Week 11</a></li>
       </ul>
     </div>
   );
