5766a67af691d61f61ebe7545fe67388e1d57df6
AndyNg555 <ngquan555@gmail.com>
2024-11-20 00:23:44 -0700

    week-9 update

--- app/week-9/page.js
+++ app/week-9/page.js
@@ -2,12 +2,11 @@
 
 import { useUserAuth } from "./_utils/auth-context";
 import { useState, useEffect } from "react";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export default function AuthComponent() {
   const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
   const [isSignedIn, setIsSignedIn] = useState(false);
-  const router = useRouter();
 
   const handleSignIn = async () => {
     await gitHubSignIn();
@@ -25,10 +24,6 @@ export default function AuthComponent() {
     }
   }, [user]);
 
-  const goToShoppingList = () => {
-    router.push("/week-9/shopping-list");
-  };
-
   return (
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-b from-gray-800 to-gray-900 text-white">
       {isSignedIn && user ? (
@@ -41,12 +36,9 @@ export default function AuthComponent() {
             >
               Sign Out
             </button>
-            <button
-              onClick={goToShoppingList}
-              className="px-4 py-2 bg-blue-500 hover:bg-blue-600 rounded"
-            >
+            <Link href="/week-9/shopping-list" className="px-4 py-2 bg-blue-500 hover:bg-blue-600 rounded">
               Go to Shopping List
-            </button>
+            </Link>
           </div>
         </div>
       ) : (
